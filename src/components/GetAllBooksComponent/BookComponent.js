89bb2d86104406f6839184fbeb6289dd6d6540fb
Aakash-suresh <MY_NAME@example.comaakashakshith455@gmail.com>
2023-06-26 10:04:52 +0530

    After adding EditBookComponent and Adding New Book Component

---
+++ src/components/GetAllBooksComponent/BookComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class BookComponent extends Component {
+  render() {
+    return (
+      <div>BookComponent</div>
+    )
+  }
+}
+
+export default BookComponent
---
+++ src/components/GetAllBooksComponent/GetAllBooksComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class GetAllBooksComponent extends Component {
+  render() {
+    return (
+      <div>GetAllBooksComponent</div>
+    )
+  }
+}
+
+export default GetAllBooksComponent
